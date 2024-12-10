126444bab9c6962954354adba212ce3426f63638
joe_tian_forexco_home_macmini <j.tian@forexco.com.au>
2024-12-10 11:57:13 +1100

    fix:fix test

--- src/test/Service.test.ts
+++ src/test/Service.test.ts
@@ -3,6 +3,6 @@ import { getCoolName } from "../app/Service";
 describe("test env setup", () => {
   test("checking", () => {
     const name = getCoolName();
-    expect(name).toBe("Cool Name11");
+    expect(name).toBe("Cool Name");
   });
 });
