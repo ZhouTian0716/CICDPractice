1201988a293e9633ccdf3de3bea1fbfb822bfce6
joe_tian_forexco_home_macmini <j.tian@forexco.com.au>
2024-12-10 12:40:54 +1100

    fix:docker test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CICDPractice
+
+```
+docker pull mongo
+```
+
+```
+docker container run --name mydatabase --publish 27017:27017 -d mongo
+```
--- src/test/Service.test.ts
+++
@@ -1,8 +0,0 @@
-import { getCoolName } from "../app/Service";
-
-describe("test env setup", () => {
-  test("checking", () => {
-    const name = getCoolName();
-    expect(name).toBe("Cool Name");
-  });
-});
